2ff5d0956a1535ffd434a217511d217170779d28
gtto05 <xander0627@outlook.com>
2021-12-03 16:19:33 +0800

    mongodb 范围筛选

--- mongodb/app.js
+++ mongodb/app.js
@@ -2,23 +2,28 @@ const db = require('./db/db')
 const stuModel = require('./db/models/stuModel')
 db(function () {
   // 创建
-  stuModel.create({
-    stu_id:'002',
-    name:'holden',
-    age:19,
-    sex:'男',
-    hobby:['coding','gym'],
-    info:'222222'
-  },(err,data) => {
-    if(!err) console.log(data);
-    console.log(err);
-  })
+  // stuModel.create({
+  //   stu_id:'002',
+  //   name:'holden',
+  //   age:19,
+  //   sex:'男',
+  //   hobby:['coding','gym'],
+  //   info:'222222'
+  // },(err,data) => {
+  //   if(!err) console.log(data);
+  //   console.log(err);
+  // })
 
   // 查找
   // stuModel.find({name:'xander'},(err,data) => {
   //   if(!err) console.log(data);
   //   console.log(err);
   // })
+  stuModel.find({age:{$gte:18}},(err,data) => {
+    if(!err) console.log(data);
+    console.log(err);
+  })
+
   // stuModel.findOne({name:'xander'},{age:1,_id:0},(err,data) => {
   //   err? console.log(err):console.log(data);
   // })
@@ -26,6 +31,11 @@ db(function () {
   // 更新
   // stuModel.updateOne({ name: 'xander' }, { age: 16 }, (err, data) => {
   //   err ? console.log(err) : console.log(data);
+  // })
+
+  // 删除 
+  // stuModel.deleteMany({age:{$gte:16}},(err,data) => {
+  //   err?console.log(err):console.log(data);
   // })
 })
 
